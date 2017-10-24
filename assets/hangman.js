9d32baf27945716af17ead339ee5bc75bbe9fb37
Anastasia Lucas <ani@theta.local>
2017-10-24 09:40:03 -0400

    DOM manipulations

--- assets/hangman.js
+++ assets/hangman.js
@@ -1,6 +1,6 @@
 //Array of monsters
 
-const monsters = ['Grootslang', 'Inkanyamba', 'Ammit', 'Aswang', 'Penanggalan', 'Diao Si Gui', 'Teke Teke', 'Ittan Momen', 'Dullahan', 'Nuckelavee', 'Kelpie', 'Arachne', 'La Ciguapa', 'Yara Ma Yha Who'];
+const monsters = ['Grootslang', 'Inkanyamba', 'Ammit', 'Aswang', 'Penanggalan', 'Diao Si Gui', 'Teke Teke', 'Ittan Momen', 'Dullahan', 'Nuckelavee', 'Kelpie', 'Arachne', 'La Ciguapa', 'Yara Ma Yha Who', 'you'];
 
 //Choose monster randomly
 let chosenMonster = monsters[Math.floor(Math.random() * monsters.length)];
@@ -13,9 +13,14 @@ let wrongGuess = [];
 //Create underscores based on length of monster name
 let underscores = [];
 
+//DOM manipulation to fill with underscores
+let underscoresDom = document.getElementsByClassName('underscores');
+let rightGuessDom = document.getElementsByClassName('rightGuess');
+
 let generateUnderscores = () => {
   for (let i = 0; i < chosenMonster.length; i++) {
-    underscores.push('_');
+    underscores.push(' _ ');
+
   }
   return underscores;
 }
@@ -32,12 +37,24 @@ document.addEventListener('keypress', (event) => {
   if (chosenMonster.indexOf(keyword) > -1) {
     console.log(rightGuess);
       rightGuess.push(keyword);
-  } else {
+       //logs letter to HTML
+        underscoresDom[0].innerHTML = underscores.join('');
+        rightGuessDom[0].innerHTML = rightGuess.join('');
+      //replace underscores with the guessed right letter
+      underscores[chosenMonster.indexOf(keyword)] = keyword;
+      console.log(underscores);
+
+      if(underscores.join('') == chosenMonster) {
+        alert('You Won!');
+      }
+
+  }  else {
     wrongGuess.push(keyword);
     console.log(wrongGuess);
   }
 });
 
+underscoresDom[0].innerHTML= generateUnderscores();
 //If it is, have it populate the underscore at it's index location as well as the Rigt Guess area
 
 //if it's not, have it populate in the wrong Guess area and decrease lives by 1
