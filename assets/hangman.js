2830798ed7b2c5fb09b84e81ad816f5fc6fab99f
Anastasia Lucas <ani@theta.local>
2017-10-24 18:39:58 -0400

    variables

--- assets/hangman.js
+++ assets/hangman.js
@@ -1,9 +1,31 @@
-//Array of monsters
+//VARIABLES FOR DAYZ ----------------------------------
 
+//Array of monsters
 const monsters = ['Grootslang', 'Inkanyamba', 'Ammit', 'Aswang', 'Penanggalan', 'Diao Si Gui', 'Teke Teke', 'Ittan Momen', 'Dullahan', 'Nuckelavee', 'Kelpie', 'Arachne', 'La Ciguapa', 'Yara Ma Yha Who', 'you'];
 
+var onceLetter = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ];
+
+//var for chosen monster
+var chosenMonster= "";
+//number of letters in monster
+var lettersInMonster = [];
+//number of blanks in monster
+var numBlanks = 0;
+//blanks and right guesses
+var blanksAndRight = [];
+//wrong guesses
+var wrongLetters = [];
+
+//scorers
+
+var wins = 0;
+var losses = 0;
+var livesLeft = 11;
+var rightGuessScore = 0;
+
+
 //Choose monster randomly
-let chosenMonster = monsters[Math.floor(Math.random() * monsters.length)];
+let chosenMonster = monsters[Math.floor(Math.random() * monsters.length)].toLowerCase();
 console.log(chosenMonster);
 
 //arrays for guesses to get pushed to
@@ -30,7 +52,7 @@ console.log(generateUnderscores());
 document.addEventListener('keypress', (event) => {
   let keycode = event.keyCode;
   /*console.log(keyCode);*/
-  let keyword = String.fromCharCode(keycode);
+  let keyword = String.fromCharCode(keycode).toLowerCase();
   console.log(keyword);
 //Check if letter guessed is in randomly chosen word- compare using indexOf
 
@@ -38,11 +60,15 @@ document.addEventListener('keypress', (event) => {
     console.log(rightGuess);
       rightGuess.push(keyword);
        //logs letter to HTML
-        underscoresDom[0].innerHTML = underscores.join('');
-        rightGuessDom[0].innerHTML = rightGuess.join('');
+
       //replace underscores with the guessed right letter
       underscores[chosenMonster.indexOf(keyword)] = keyword;
       console.log(underscores);
+
+      underscoresDom[0].innerHTML = underscores.join(" ");
+      rightGuessDom[0].innerHTML = rightGuess.join(" ");
+
+
 
       if(underscores.join('') == chosenMonster) {
         alert('You Won!');
