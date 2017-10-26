3b1ca29a445a96d1f1b3f82ec7ffaf5c6c030c01
Anastasia Lucas <ani@theta.local>
2017-10-25 20:22:45 -0400

    message object

--- assets/hangman.js
+++ assets/hangman.js
@@ -96,6 +96,10 @@ function pickPic(){
   document.getElementById('monsterPicture').src = picObject[chosenMonster];
 };
 
+function pickMessage(){
+  document.getElementById('message').innerHTML = messageObject[chosenMonster];
+};
+
 function winLose() {
   //win when number of blanks filled with right word
   if(rightGuessScore === numBlanks) {
@@ -105,6 +109,7 @@ function winLose() {
     // alert('You Win!');
     // alert(chosenMonster);
     pickPic()
+    pickMessage()
   } else if (guessesLeft === 0) {
     losses--;
 
@@ -145,6 +150,24 @@ var picObject = {
 
 
 // pickPic();``
+
+//messages Object
+var messageObject = {
+  "grootslang" : "<h3>Grootslang</h3><br><strong>South Africa</strong><br> Massive serpentine elephant that plagues a deep cave in Richtersveld.",
+  "inkanyamba" : "<h3>Inkanyamba</h3><br><strong>South Africa</strong><br> Gigantic, winged ell with a voracious appetite that is associated with brutal storms.",
+  "ammit" :  "<h3>Ammit</h3><br><strong>Egypt</strong><br>Goddess with a lion, hippo, and crocodile body. She eats the impure hearts of the souls of the deat, condemning them to be restless forever.",
+  "aswang" : "<h3><Aswang></h3><br><strong>Philippines</strong><br>Shape-shifting monster that devours unborn babies. To spot one in the daytime, look for your upside-down reflection in their eyes.",
+  "penanggalan": "<h3>Penanggalan</h3><br><strong>Malaysia</strong><br>Detached female head that flies through the night, organs dangling, to search for blood. She reattaches to the rest of her body during the day.",
+  "teke teke":  "<h3>Teke Teke</h3><br><strong>Japan</strong><br>Ghost of a young girl who was cut in half by a train. She drags her torso around, seeking to dismember others with a scythe.",
+  "ittan momen": "<h3>Ittan Momen</h3><br><strong>Japan</strong><br>Sentient roll of cotton that smothers people until they suffocate.",
+  "dullahan": "<h3>Dullahan</h3><br><strong>Ireland</strong><br>Headless rider that uses a human spine as a whip. If one stops riding next to you and calls out your name, you die instantly.",
+  "nuckelavee": "<h3>Nuckelavee</h3><br><strong>Orkney</strong><br>Horrific demon that appears as a skinned horse and male torso hybrid and breathes disease and decay.",
+  "kelpie": "<h3>Kelpie</h3><br><strong>Scotland</strong><br>Emits a cry that sounds like a drowning person to lure prey into the water, then plunges them into an aquatic grave.",
+  "arachne": "<h3>Arachne</h3><br><strong>Greece</strong><br>Weaver who challenged Athena to a contest but was punished for her hubris by being turned into a spider.",
+  "la ciguapa": "<h3>La Ciguapa</h3><br><strong>Dominican Republic</strong><br>Enchanting woman with backward feet that lures men into the woods.",
+  "yara ma yha who": "<h3>Yara Ma Yha Who</h3><br><strong>Australia</strong><br>Short, red vampire that drinks blood through ocotopus-like suckers on its hands and feet. After a nap, it will vomit out the meal.",
+
+}
 //start the game
 startGame();
 
